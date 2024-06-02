0a63a5ef69972848fdf79f326886f2b0c89b2e76
zidan970 <amdzidan70@gmail.com>
2024-06-02 19:56:39 +0700

    class customer

---
+++ Customer.java
@@ -0,0 +1,32 @@
+class Customer {
+    private String nama;
+    private String alamat;
+    private EWallet ewallet;
+
+    // set get nama
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    // set get alamat
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    // set get ewallet
+    public void setEWallet(EWallet ewallet) {
+        this.ewallet = ewallet;
+    }
+
+    public EWallet getEWallet() {
+        return ewallet;
+    }
+}
