13e143acaeb28100d14a46858fd2d22a04408106
zidan970 <amdzidan70@gmail.com>
2024-06-03 13:31:19 +0700

    update object E-Wallet and transaction

--- CustomerMain.java
+++ CustomerMain.java
@@ -8,24 +8,45 @@ public class CustomerMain {
         customer.setAlamat("sendai city");
         // customer.getEWallet().setSaldo(5000);
 
+        // create object
+        Customer customer2 = new Customer();
+        customer2.setNama("kugisaki nobara");
+        customer2.setAlamat("tokyo");
+
         // object ewallet
         EWallet ewallet = new EWallet();
         ewallet.setSaldo(5000);
         customer.setEWallet(ewallet);
 
+        // object ewallet
+        EWallet ewallet2 = new EWallet();
+        ewallet2.setSaldo(5000);
+        customer2.setEWallet(ewallet2);
+
         // print data customer
         System.out.println("Nama: " + customer.getNama());
         System.out.println("Alamat: " + customer.getAlamat());
-        System.out.println("Saldo: " + customer.getEWallet().cekSaldo());
+        System.out.println("Saldo saat ini: " + customer.getEWallet().cekSaldo()+"\n");
+
+        // print data customer
+        System.out.println("Nama: " + customer2.getNama());
+        System.out.println("Alamat: " + customer2.getAlamat());
+        System.out.println("Saldo saat ini: " + customer2.getEWallet().cekSaldo()+"\n");
 
         // topup
         customer.getEWallet().topup(2500.0, "2024-06-02", "ABC456", "Credit Card");
-        System.out.println("Saldo terbaru: " + customer.getEWallet().cekSaldo());
+        System.out.println("Saldo " + customer.getNama() + " setelah topup: " + customer.getEWallet().cekSaldo());
 
-        // Mencetak transaksi yang dilakukan oleh pelanggan
-        //System.out.println("Transaksi yang dilakukan oleh " + customer.getNama() + ":");
+        // transfer
+        customer.getEWallet().transfer(customer2, 500.0, "2024-06-02");
+        System.out.println("Saldo " + customer.getNama() + " setelah transfer: " + customer.getEWallet().cekSaldo());
+        System.out.println("Saldo " + customer2.getNama() + " setelah ditransfer: " + customer2.getEWallet().cekSaldo());
+
+        // daftar transaksi
+        System.out.println("\n" + "Daftar Transaksi:");
         List<Transaction> transaksi = customer.getEWallet().listTransactions();
         for (Transaction elemen : transaksi) {
-            elemen.cetakInformasiTransaksi(customer.getNama());        }
+            elemen.cetakInformasiTransaksi(customer.getNama());
+        }
     }
 }
